f26fe605d3908902d8ff49bae15ba1254758739f
SriVenkat77 <167739518+SriVenkat77@users.noreply.github.com>
2024-10-21 21:31:38 +0530

    Update ResetPaswordRequest.jsx

--- src/customers/pages/Auth/ResetPaswordRequest.jsx
+++ src/customers/pages/Auth/ResetPaswordRequest.jsx
@@ -11,7 +11,6 @@ import {
   Backdrop,
   CircularProgress,
 } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
   
